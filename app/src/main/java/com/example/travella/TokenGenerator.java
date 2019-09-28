199bdf7655e1c30f78b3878595883a9e72999286
chathura <jrcmadushanka@gmail.com>
2019-09-28 12:33:48 +0530

    Working api

--- app/src/main/java/com/example/travella/TokenGenerator.java
+++ app/src/main/java/com/example/travella/TokenGenerator.java
@@ -10,6 +10,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.zxing.BarcodeFormat;
@@ -20,8 +21,13 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TokenGenerator extends AppCompatActivity {
+
     ImageView tokenPreview;
 
     @Override
@@ -31,35 +37,35 @@ public class TokenGenerator extends AppCompatActivity {
 
         tokenPreview = findViewById(R.id.tokenView);
 
-        getToken();
+        generateToken("12", "2");
 
     }
 
-    private void getToken (){
-        String text = "";
+    private void generateToken(String id, String journey){
 
-        String json_url = "https://powerful-plateau-81192.herokuapp.com/api/vehicles";
+        Map<String, String> params = new HashMap<String, String>();
 
-        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, json_url, null, new Response.Listener<JSONArray>() {
+        params.put("journey_id", journey);
+        params.put("user_id", id);
+
+        String json_url = "https://powerful-plateau-81192.herokuapp.com/api/token/";
+
+        JSONObject jsonObject = new JSONObject(params);
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, json_url, jsonObject, new Response.Listener<JSONObject>() {
 
             @Override
-            public void onResponse(JSONArray array) {
-                String[] id = new String[array.length()];
-                String[] number = new String[array.length()];
-                String[] facility = new String[array.length()];
-                String[] route = new String[array.length()];
+            public void onResponse(JSONObject array) {
 
-                    try {
-                        array.getString(0);
-                        id[0] = (array.getJSONObject(0).get("id")).toString();
-                        number[0] = (array.getJSONObject(0).get("number")).toString();
-                        facility[0] = (array.getJSONObject(0).get("facility")).toString();
-                        route[0] = (array.getJSONObject(0).get("routId")).toString();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                String[] url = new String[array.length()];
 
-                    generateQr(number[0]);
+                try {
+                    url[0] = (array.get("token")).toString();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                generateQr(url[0]);
 
             }
 
@@ -70,35 +76,11 @@ public class TokenGenerator extends AppCompatActivity {
             }
         });
         Volley.newRequestQueue(TokenGenerator.this).add(jsonObjectRequest);
-
-/*      ret
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);*/
-
     }
 
     private void generateQr(String text){
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try {
-            System.out.println("on  gene ++++++++++++++++++++++++++++");
             BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,400,400);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
             Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
